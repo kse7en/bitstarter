a7633ae19e1e5cc85595d641c68097aa599faaf2
kse7en <kl@kl.io>
2013-07-07 07:07:21 +0000

    removed typo on web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 var indexFile = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('indexFile.toString()');
+  response.send(indexFile.toString());
 });
 
 var port = process.env.PORT || 5000;
