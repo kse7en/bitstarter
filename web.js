4c49ac976dea155a2c02713abcc3ba0ea6341c94
kse7en <kl@kl.io>
2013-08-29 07:54:04 +0000

    changed about section

--- web.js
+++ web.js
@@ -4,9 +4,18 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var indexFile = fs.readFileSync('index.html');
+var aboutFile = fs.readFileSync('about.html');
 
 app.get('/', function(request, response) {
   response.send(indexFile.toString());
+});
+
+app.get('/index.html', function(request, response) {
+  response.send(indexFile.toString());
+});
+
+app.get('/about.html', function(request, response) {
+  response.send(aboutFile.toString());
 });
 
 var port = process.env.PORT || 8080;
